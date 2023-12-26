ee3e28314eda83e8befa5b9da8195ae4eaddebbf
Alejandro Front <desarrollo@qsocialnow.com>
2023-12-26 10:41:45 -0300

    cambio grafos a movilizacion-DNU

--- src/componentes/Graficos/GrafoHashtag.jsx
+++ src/componentes/Graficos/GrafoHashtag.jsx
@@ -47,7 +47,7 @@ export default function GraphHashtags(){
     <div className='grafo-video'>
     
     <Tooltip title="Click para ver el grafo">
-    <a href={`https://qsngrafos.vercel.app/co-ocurrencia/movilizacion/grafo_co-ocurencia_hashtags-${filtroFecha}.html`} target="_blank">
+    <a href={`https://qsngrafos.vercel.app/co-ocurrencia/movilizacion-DNU/grafo_co-ocurencia_hashtags-${filtroFecha}.html`} target="_blank">
     <div className='video-explicativo cartaGrafo'>
       <img src={imagen} className='imagen-grafo' />
     </div>
